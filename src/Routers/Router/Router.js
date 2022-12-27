5e160ee1c7e91d6fe48251493fc6f53041503994
kowsarahammed80 <kowsarahammed80@gmail.com>
2022-12-27 18:44:03 +0600

    register-design-done

--- src/Routers/Router/Router.js
+++ src/Routers/Router/Router.js
@@ -1,18 +1,36 @@
 import { createBrowserRouter } from "react-router-dom";
+import Loging from "../../Components/Loging/Loging";
+import Register from "../../Components/Register/Register";
 import Main from "../../Layout/Main/Main";
 import Homepages from "../../Pages/HomePages/Homepages";
 
 const routers = createBrowserRouter([
+  
+
   {
     path: '/',
-    element: <Main></Main>,
-    children: [
-      {
-        path: '/',
-        element: <Homepages></Homepages>
-      }
-    ]
-  }
+    element: <Loging></Loging> 
+  },
+
+  {
+    path: '/register',
+    element: <Register></Register>
+  },
+
+  
+
+   
+  // {
+  //   path: '/',
+  //   element: <Main></Main>,
+  //   children: [
+  //     {
+  //       path: '/home',
+  //       element: <Homepages></Homepages>
+  //     }
+  //   ]
+    
+  // },
 ])
 
 export default routers;
